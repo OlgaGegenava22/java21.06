db8fb898161225c16f8b25079965f752ca9826d7
Ольга Гегенава <zaharova.mail@mail.ru>
2022-06-23 13:54:26 +0700

    1 версия

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20_000;
+        int oneMiles = 20;
+
+        int miles = price / oneMiles;
+        System.out.println("Бонусные милли: " + miles);
+
+
+    }
+}
